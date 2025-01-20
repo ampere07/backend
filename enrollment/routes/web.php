a548ab7e1396b41f816f9d1da22eaad10a2f8a93
ampere07 <ravenampere0123@gmail.com>
2025-01-20 14:29:16 -0800

    a

--- enrollment/routes/web.php
+++ enrollment/routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\DB;
 
-Route::get('/test', function () {
-    try {
-        // Check if database is connected
-        DB::connection()->getPdo();
-        return response()->json(['message' => 'Backend is running and database is connected']);
-    } catch (\Exception $e) {
-        // Log the error message
-        Log::error('Database connection failed: ' . $e->getMessage());
-
-        // Return a response indicating the error
-        return response()->json(['message' => 'Backend is running, but database connection failed'], 500);
-    }
+Route::get('/', function () {
+    return view('welcome');
 });
