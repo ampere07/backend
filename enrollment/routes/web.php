410be075c1ba81f339d0c306ad72514b20ea0230
ampere07 <ravenampere0123@gmail.com>
2025-01-20 16:26:26 -0800

    a

--- enrollment/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('api')->group(function () {
-    Route::get('/test', function () {
-        return response()->json(['message' => 'Backend is running']);
-    });
-});
